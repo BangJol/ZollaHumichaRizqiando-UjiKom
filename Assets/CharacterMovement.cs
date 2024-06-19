687f7c6a5abb14a75ad6fba9ce2c0bd8c2dd873d
Zolla Humicha <zollahr27@gmail.com>
2024-06-19 09:47:09 +0700

    Animation Player
    Strafe Right & Left Animation

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -11,33 +11,60 @@ public class CharacterMovement : MonoBehaviour
     public bool isMoving;
     public float playerSpeed;
     public float gravityValue = -9.81f;
+    public Animator animator;
 
     private void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Movement();
+        Throwing();
     }
 
     public void Movement()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Left", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Left", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Right", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Right", false);
+        }
 
         if (move != Vector3.zero)
         {
             gameObject.transform.position = move;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Instantiate(food, ,);
-        }
-
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
+
+    public void Throwing()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Throw");
+        }
+    }
+
 }
